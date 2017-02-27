95557cf18092a12cfdb3e74ce1dc61b56c4d9f42
chuglas <doug@wearecirro.com>
2017-02-27 17:57:23 +0100

    gitignore

--- starter-code/iteration-3/interfaces.ts
+++ starter-code/iteration-3/interfaces.ts
@@ -4,9 +4,28 @@
 // - A property updatedAt, obiously is a date.
 // - Method for toogle if it's done or not, doesn't return anything.
 
+export interface TodoDoItemInterface {
+    title: string;
+    status: boolean;
+    listAllTasks(): void;
+    deleteTask(task: string): number;
+}
+
+
+
+
+
 // Write the interface for class Todo. It must have:
 // - An array of TodoItem as a property.
 // - Method for adding task where will receive an instance of TodoItem, doesn't return anything.
 // - Method for listing all task in the console, doesn't return anything.
 // - Method for delete a task where will receive an instance of TodoItem, doesn't return anything.
 // - Method for show all tasks that are not yet done, doesn't return anything.
+
+
+export interface ToDoInterface {
+    myArray: Array<string>;
+    addTask(task: string): number;
+    listAllTasks(): void;
+    deleteTask(task: string): number;
+}
